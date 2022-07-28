a63eca3306ffeae7b20db2329066bd5b7c408124
Robert Shepley <shepleysound@gmail.com>
2022-07-28 15:48:48 -0700

    Add localStorage feature.

--- js/app.js
+++ js/app.js
@@ -24,11 +24,14 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(this.items))
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  const existingItem = this.items.find(cartItem => cartItem.product === item)
+  console.log(existingItem)
 };
 
 const CartItem = function(product, quantity) {
@@ -68,5 +71,3 @@ function generateCatalog() {
 
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
-
-console.log(Product.allProducts)
